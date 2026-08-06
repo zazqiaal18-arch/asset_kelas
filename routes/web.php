5bc090d9093b5ce28c6c58c1e7b114c665239974
aqilla14 <aqilla.oktaviani214@smk.belajar.id>
2026-08-06 13:50:06 +0700

    buat route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\StokController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PenyusutanController;
+use App\Http\Controllers\KerusakanController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +38,25 @@ Route::post('/stok', [StokController::class, 'store'])->name('stok.store');
 Route::get('/stok/{id_stok}/edit', [StokController::class, 'edit'])->name('stok.edit');
 Route::put('/stok/{id_stok}', [StokController::class, 'update'])->name('stok.update');
 Route::delete('/stok/{id_stok}', [StokController::class, 'destroy'])->name('stok.destroy');
+
+// ROUTE KATEGORI BARANG
+Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
+Route::get('/kategori/{id_kategori}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::put('/kategori/{id_kategori}', [KategoriController::class, 'update'])->name('kategori.update');
+Route::delete('/kategori/{id_kategori}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+
+// 4. ROUTE MASA EKONOMIS & PENYUSUTAN
+Route::get('/penyusutan', [PenyusutanController::class, 'index'])->name('penyusutan.index');
+Route::get('/penyusutan/create', [PenyusutanController::class, 'create'])->name('penyusutan.create');
+Route::post('/penyusutan', [PenyusutanController::class, 'store'])->name('penyusutan.store');
+Route::delete('/penyusutan/{id_penyusutan}', [PenyusutanController::class, 'destroy'])->name('penyusutan.destroy');
+
+// 5. ROUTE KERUSAKAN BARANG
+Route::get('/kerusakan', [KerusakanController::class, 'index'])->name('kerusakan.index');
+Route::get('/kerusakan/create', [KerusakanController::class, 'create'])->name('kerusakan.create');
+Route::post('/kerusakan', [KerusakanController::class, 'store'])->name('kerusakan.store');
+Route::get('/kerusakan/{id_kerusakan}/edit', [KerusakanController::class, 'edit'])->name('kerusakan.edit');
+Route::put('/kerusakan/{id_kerusakan}', [KerusakanController::class, 'update'])->name('kerusakan.update');
+Route::delete('/kerusakan/{id_kerusakan}', [KerusakanController::class, 'destroy'])->name('kerusakan.destroy');
