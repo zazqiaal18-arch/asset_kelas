f7d5555640b0b52b3148fed1fab24adfaa1ebe48
zazqiaal18-arch <zazqia.al18@smk.belajar.id>
2026-08-13 09:31:23 +0700

    menambahkan dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\StokController;
 use App\Http\Controllers\KategoriController;
